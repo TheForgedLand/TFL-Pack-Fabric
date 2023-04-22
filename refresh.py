2f7aa24213b5be29a1687f183390361e7fa68953
AliCCfire <alicecfire@gmail.com>
2023-04-22 17:18:58 -0300

    fix index

--- refresh.py
+++ refresh.py
@@ -3,5 +3,5 @@ import toml
 import os
 os.system("packwiz refresh")
 with open("pack-config/flavorgroups.toml", "r") as f, open("pack-config/flavors.yaml") as fl:
-    data = toml.load(f) | {"metafile": {k: {"flavors": v.split(",")} for k, v in yaml.safe_load(fl).items()}} # type: ignore
+    data = toml.load(f) | {"metafile": {k: {"flavors": v} for k, v in yaml.safe_load(fl).items()}} # type: ignore
 with open("unsup.toml", "w") as f: toml.dump(data, f)
