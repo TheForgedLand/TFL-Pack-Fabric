c350cef949020ca265b02f8ba1c9dd6e756f12bc
AliCCfire <97907169+AliCCfire@users.noreply.github.com>
2023-04-27 10:52:10 -0300

    Update refresh.py

--- refresh.py
+++ refresh.py
@@ -1,6 +1,10 @@
 import yaml
 import toml
 import os
+import tempfile
+for x in os.listdir("./mods"):
+    x.replace(".pw.toml", "")
+
 os.system("packwiz refresh")
 with open("pack-config/flavorgroups.toml", "r") as f, open("pack-config/flavors.yaml") as fl:
     data = toml.load(f) | {"metafile": {k: {"flavors": v.split(",")} for k, v in yaml.safe_load(fl).items()}} # type: ignore
