9bb294988dfd4302645e9110fd817ae0b8938311
AliCCfire <97907169+AliCCfire@users.noreply.github.com>
2023-04-27 12:11:59 -0300

    Update refresh.py

--- refresh.py
+++ refresh.py
@@ -4,8 +4,8 @@ modlist = [modname.replace(".pw.toml", "") for modname in os.listdir("./mods")]
 import toml
 import yaml
 with open("pack-config/flavorgroups.toml", "r") as f, open("pack-config/flavors.yaml") as fl:
-    mods = {modname: "misc_on" for modname in modlist}|yaml.safe_load(fl)
-    yaml.dump(mods,fl)
+    modflavors = {modname: "misc_on" for modname in modlist}|yaml.safe_load(fl)
+    yaml.dump(modflavors,fl)
     data = toml.load(f) | {"metafile": {k: {"flavors": v.split(",")} for k, v in yaml.safe_load(fl).items()}} # type: ignore
 with open("unsup.toml", "w") as f: toml.dump(data, f)
 os.system("packwiz refresh")
