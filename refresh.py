07b30df1284db2ac58ca2f671e68741670c43805
AliCCfire <alicecfire@gmail.com>
2023-05-07 23:40:49 -0300

    update flavour system

--- refresh.py
+++ refresh.py
@@ -1,7 +1,25 @@
 import yaml
 import toml
 import os
+import Levenshtein
+
+f,fl = open("pack-config/flavorgroups.toml", "r"), yaml.safe_load(open("pack-config/flavors.yaml"))
+modlist = [modname.replace(".pw.toml", "") for modname in os.listdir("./mods")]
+modflavors = {modname: "misc_on" for modname in modlist} | fl  # type: ignore
+
+data = toml.load(f) | {"metafile": {k: {"flavors": v} for k, v in fl.items()}} # type: ignore
+outdated = []
+for mod in modflavors.keys():
+    if mod not in modlist:
+        outdated.append(mod)
+print("These mods are being removed!!\n",outdated)
+for mod in outdated:
+    modflavors.pop(mod)
+    for m in modlist:
+        if Levenshtein.ratio(mod,m)>=0.85: print(mod,"is probably related to",m)
+
+
+with open("unsup.toml", "w") as f, open("pack-config/flavors.yaml","w") as fl:
+    yaml.dump(modflavors, fl)
+    toml.dump(data, f)
 os.system("packwiz refresh")
-with open("pack-config/flavorgroups.toml", "r") as f, open("pack-config/flavors.yaml") as fl:
-    data = toml.load(f) | {"metafile": {k: {"flavors": v.split(",")} for k, v in yaml.safe_load(fl).items()}} # type: ignore
-with open("unsup.toml", "w") as f: toml.dump(data, f)
