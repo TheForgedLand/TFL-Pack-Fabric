f7bd6e3f75bdaa42b1153a235d71ba8d46ccf8ea
AliCCfire <alicecfire@gmail.com>
2023-08-17 10:44:39 -0300

    - Update mods - farmer's knives back to cf project - add recipe overrides

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -2,6 +2,20 @@
 ServerEvents.recipes(event => {
 	// Change recipes here
 
+	event.remove({input: "create:dough"})
+	event.replaceOutput(
+		{ output: "create:dough" },
+		"create:dough",
+		"farmersdelight:wheat_dough"
+	)
+
+	event.replaceInput(
+		{id: "minecraft:writable_book"},
+		"minecraft:ink_sac",
+		"minecraft:black_dye"
+	)
+	event.smelting("minecraft:leather", "minecraft:rotten_flesh")
+
 	event.remove({ id: "minecraft:chain" })
 	event.remove({ id: "architects_palette:nether_brass_chain" })
 	let chain = (id, metal_id) => {
@@ -18,8 +32,8 @@ ServerEvents.recipes(event => {
 	chain("minecraft:chain", "minecraft:iron")
 	chain("architects_palette:nether_brass_chain", "architects_palette:nether_brass")
 
-	event.remove({ output: "expandeddelight:cheese_wheel" })
 
+	event.remove({ output: "expandeddelight:cheese_wheel" })
 	event.replaceInput(
 		{ input: "expandeddelight:cheese_slice" },
 		"expandeddelight:cheese_slice",
